79c4aa3a6231cf5559d1f0d8afa2f7955734607d
jasurkurbanovinit <jasurkurbanov96@gmail.com>
2020-12-26 11:32:35 +0500

    graphql added

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    title: `Title from siteMetadata2`,
+  },
+  plugins: [
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
+}
